f31b36a89e6164e8006eb868919900b0f4f8df1d
Sunny Suri <suri.sunny0@gmail.com>
2025-09-27 23:03:37 +0530

    Add headless options for all browsers; update testng.xml with new tests

--- src/main/java/com/mystorev1/TestBase.java
+++ src/main/java/com/mystorev1/TestBase.java
@@ -6,8 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -28,33 +31,57 @@ public class TestBase {
         return driver;
     }
 
-    @BeforeMethod
-    public void setup(ITestContext context) {
-        switch (browserName.toLowerCase()) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browserName);
-        }
+  @BeforeMethod
+public void setup(ITestContext context) {
+    boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "false"));
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.get(url);
+    switch (browserName.toLowerCase()) {
+        case "chrome":
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            if (isHeadless) {
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+            }
+            driver = new ChromeDriver(chromeOptions);
+            break;
 
-        context.setAttribute("driver", driver);
+        case "edge":
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions edgeOptions = new EdgeOptions();
+            if (isHeadless) {
+                edgeOptions.addArguments("--headless=new");
+                edgeOptions.addArguments("--disable-gpu");
+                edgeOptions.addArguments("--window-size=1920,1080");
+            }
+            driver = new EdgeDriver(edgeOptions);
+            break;
 
-        log.info("Base URL is opened in " + browserName);
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            if (isHeadless) {
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+            }
+            driver = new FirefoxDriver(firefoxOptions);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unsupported browser: " + browserName);
     }
+
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    driver.get(url);
+
+    context.setAttribute("driver", driver);
+
+    log.info("Base URL is opened in " + browserName + (isHeadless ? " (headless)" : ""));
+}
+
 
     @AfterMethod
     public void tearDown() {
